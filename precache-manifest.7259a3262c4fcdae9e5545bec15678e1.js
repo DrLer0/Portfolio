28ccdb4824305dd1ca3e109ac419440227d24293
Jonathan Doctolero <jonathan.doctolero7@gmail.com>
2020-05-22 14:46:52 -0700

    Updates

---
+++ precache-manifest.7259a3262c4fcdae9e5545bec15678e1.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a8384c241bf5a50f722f08d65484c8",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "1ee0e78c326985d58ee5",
+    "url": "/Portfolio/static/css/main.14e8bdd0.chunk.css"
+  },
+  {
+    "revision": "38a6da74e88df2317e66",
+    "url": "/Portfolio/static/js/2.18ad4046.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio/static/js/2.18ad4046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ee0e78c326985d58ee5",
+    "url": "/Portfolio/static/js/main.0dff26d1.chunk.js"
+  },
+  {
+    "revision": "425e952245b014fc3ffb",
+    "url": "/Portfolio/static/js/runtime-main.db605a1d.js"
+  },
+  {
+    "revision": "882c74d33834441894db0419c3afcc53",
+    "url": "/Portfolio/static/media/Crystals.882c74d3.png"
+  },
+  {
+    "revision": "53c386eacb6142baad275fdb0d538292",
+    "url": "/Portfolio/static/media/FruitGifs.53c386ea.png"
+  },
+  {
+    "revision": "92d5047b4484bc4314405cec03674453",
+    "url": "/Portfolio/static/media/Hangman.92d5047b.png"
+  },
+  {
+    "revision": "2dea63910cd1a162470e65c73886a0dc",
+    "url": "/Portfolio/static/media/RecipesFinder.2dea6391.png"
+  },
+  {
+    "revision": "e569da9e3a944a164382b94037781135",
+    "url": "/Portfolio/static/media/Trivia.e569da9e.png"
+  },
+  {
+    "revision": "cb44ca53c97bdbe686c2b6a2d6c5f977",
+    "url": "/Portfolio/static/media/ac-book.cb44ca53.png"
+  },
+  {
+    "revision": "4b14118cc9b63dc7c5936bfa78e76b18",
+    "url": "/Portfolio/static/media/bmazon.4b14118c.png"
+  },
+  {
+    "revision": "619b66ad17b1b751616ae006787d0501",
+    "url": "/Portfolio/static/media/burger.619b66ad.png"
+  },
+  {
+    "revision": "bad6fd630b1ffcb3062aefce6d827669",
+    "url": "/Portfolio/static/media/friendfinder.bad6fd63.png"
+  },
+  {
+    "revision": "61f80f18c8beb4c9fed5cc25b0090ca6",
+    "url": "/Portfolio/static/media/harrypotterrpg.61f80f18.png"
+  },
+  {
+    "revision": "a347e8f498852580de089b91af714d51",
+    "url": "/Portfolio/static/media/headshot.a347e8f4.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "de058059a18e2111aaa5d8114e396863",
+    "url": "/Portfolio/static/media/newsprint.de058059.jpg"
+  },
+  {
+    "revision": "c90abd960721d40cba4bb5747fea5e00",
+    "url": "/Portfolio/static/media/reactGoogle.c90abd96.png"
+  },
+  {
+    "revision": "b8800fbc7569255fc78f7a853c934d3b",
+    "url": "/Portfolio/static/media/rock.b8800fbc.png"
+  },
+  {
+    "revision": "3baec3067527d632f0d8204e0c223066",
+    "url": "/Portfolio/static/media/spotify-this-song.3baec306.png"
+  }
+]);
